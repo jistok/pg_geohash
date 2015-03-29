f37e31a8a2c8c76c14ecb0f7bf4b41fee697c315
Michael Goddard <mgoddard@pivotal.io>
2015-03-29 09:32:30 -0400

    Add OR REPLACE to SQL function def

--- pg_geohash-1.0.sql
+++ pg_geohash-1.0.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION geohash_lat_lon (float8, float8) RETURNS text
+CREATE OR REPLACE FUNCTION geohash_lat_lon (float8, float8) RETURNS text
   AS 'pg_geohash', 'geohash_lat_lon'
   LANGUAGE C;
 
