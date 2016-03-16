f28f8ca25f4de69d28f231af55596b1bd388a711
Michael Goddard <mgoddard@gopivotal.com>
2016-03-16 08:11:54 -0400

    Marking these as IMMUTABLE since this helps the Optimizer

--- pg_geohash-1.0.sql
+++ pg_geohash-1.0.sql
@@ -24,9 +24,11 @@
 
 CREATE OR REPLACE FUNCTION lat_lon_to_geohash (float8, float8) RETURNS text
   AS 'pg_geohash', 'lat_lon_to_geohash'
+  IMMUTABLE
   LANGUAGE C;
 
 CREATE OR REPLACE FUNCTION geohash_to_lat_lon (text) RETURNS text
   AS 'pg_geohash', 'geohash_to_lat_lon'
+  IMMUTABLE
   LANGUAGE C;
 
